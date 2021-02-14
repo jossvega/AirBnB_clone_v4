3d977c58afb9b1e7eb569c04979137d37ee377c1
Victor Zuluaga <zrvictor@outlook.com>
2021-02-14 18:29:10 -0500

    DOM maniulation jQuery

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,25 @@
+const myArray = [];
+$(document).ready(function () {
+  $('input:checkbox').change(function () {
+    if ($(this).is(':checked')) {
+      const item = {};
+      item.id = $(this).attr('data-id');
+      item.name = $(this).attr('data-name');
+      myArray[item.id] = item.name;
+    } else {
+      const item = {};
+      item.id = $(this).attr('data-id');
+      delete myArray[item.id];
+    }
+    $('div.amenities h4').text(Object.values(myArray).join(', '));
+  });
+});
+
+// get status API
+$.get('http://127.0.0.1:5001/api/v1/status/', function (data) {
+  if (`${data.status}` === 'OK') {
+    $('#api_status').addClass('available');
+  } else {
+    $('#api_status').removeClass('available');
+  }
+});
