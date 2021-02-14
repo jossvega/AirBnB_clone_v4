b87b5a334620f0c79afe7c2ff27c34a92948b5a3
Victor Zuluaga <zrvictor@outlook.com>
2021-02-14 15:55:57 -0500

    fix display h4 in ellipsis content

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,16 +1,24 @@
-let myArray = []
+const myArray = [];
 $(document).ready(function () {
-  $('input:checkbox').change(function() {
-      if ($(this).is(':checked')) {
-        let item = {}
-        item.id = $(this).attr('data-id')
-        item.name = $(this).attr('data-name')
-        myArray[item.id] = item.name
-      } else {
-        let item = {}
-        item.id = $(this).attr('data-id')
-        delete myArray[item.id]
-      }
-      $('div.amenities h4').text(Object.values(myArray).join(', '))
+  $('input:checkbox').change(function () {
+    if ($(this).is(':checked')) {
+      const item = {};
+      item.id = $(this).attr('data-id');
+      item.name = $(this).attr('data-name');
+      myArray[item.id] = item.name;
+    } else {
+      const item = {};
+      item.id = $(this).attr('data-id');
+      delete myArray[item.id];
+    }
+    $('div.amenities h4').text(Object.values(myArray).join(', '));
+    $('.filters h4').css({
+      display: 'inline-block',
+      width: '200px',
+      height: '16px',
+      'white-space': 'nowrap',
+      overflow: 'hidden',
+      'text-overflow': 'ellipsis'
+    });
   });
 });
