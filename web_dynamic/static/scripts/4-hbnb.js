dfb23e0d4dfa6444b9745dfeb60eed924adae7d7
jossvega <jossvega103@gmail.com>
2021-02-15 20:30:51 -0500

    Add task 5. Filter places by Amenity

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -57,5 +57,31 @@ $.ajax({
 
 // event click for search
 $('button').click(function () {
-  
+  $('button').on('click', function () {
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      dataType: 'json',
+      contentType: 'application/json',
+      data: JSON.stringify({'amenities': Object.keys(amenities)}),
+      success: function (response) {
+        for (let i = 0; i < response.length; i++) {
+          $('.places').append(
+            '<article>' +
+            '<div class="title_box">' +
+              '<h2>' + response[i].name + '</h2>' +
+              '<div class="price_by_night">$' + response[i].price_by_night + '</div>' +
+            '</div>' +
+            '<div class="information">' +
+              '<div class="max_guest">' + response[i].max_guest + ' Guest</div>' +
+                    '<div class="number_rooms">' + response[i].number_rooms + ' Bedroom</div>' +
+                    '<div class="number_bathrooms">' + response[i].number_bathrooms + ' Bathroom</div>' +
+            '</div>' +
+            '<div class="user">' +
+          '</article>'
+          );
+        }
+      }
+    });
+  });
 });
